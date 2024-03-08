e22573d3f69630e734c802d55cf2b8d32aeb2167
kalkidan yishak <kalkidanyishak002@gmail.com>
2024-03-08 13:00:52 +0300

    initail commit

---
+++ librarySystem/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    role = models.CharField(max_length=100)
+    is_banned = models.BooleanField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    STATUS_CHOICES = [
+        ('AVAILABLE', 'Available'),
+        ('BORROWED', 'Borrowed'),
+        ('RESERVED', 'Reserved'),
+    ]
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    average_rating = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Loan(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrowed_date = models.DateTimeField()
+    returned_date = models.DateTimeField(null=True, blank=True)
+
+class Review(models.Model):
+    content = models.TextField()
+    rating = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
