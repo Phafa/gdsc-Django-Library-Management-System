b02239acb85225c75f93c112c0217bb1446b8be0
kalkidan yishak <kalkidanyishak002@gmail.com>
2024-03-08 16:12:06 +0300

    changed loan class to borrowed book in models.py because there was over all change in the db schema. \nchanged user field to student for ease. changed review content to review_text for better code readability. \nqouted classes in the forienKey because it is a good practice

--- librarySystem/models.py
+++ librarySystem/models.py
@@ -9,29 +9,47 @@ class User(models.Model):
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     role = models.CharField(max_length=100)
-    is_banned = models.BooleanField()
-
+    date_joined = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.username
 
 class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    genre = models.CharField(max_length=100)
-    STATUS_CHOICES = [
-        ('AVAILABLE', 'Available'),
-        ('BORROWED', 'Borrowed'),
-        ('RESERVED', 'Reserved'),
-    ]
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True, default=None)
+    number_of_copies = models.IntegerField()
+    currently_available_copies = models.IntegerField()
     average_rating = models.DecimalField(max_digits=5, decimal_places=2)
 
-class Loan(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class BorrowedBook(models.Model):
+    borrowed_id = models.AutoField(primary_key=True)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    student = models.ForeignKey('User', on_delete=models.CASCADE)
     borrowed_date = models.DateTimeField()
     returned_date = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.student} -> {self.book}"
+
 class Review(models.Model):
-    content = models.TextField()
-    rating = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    review_id = models.AutoField(primary_key=True)
+    review_text = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])   
+    student = models.ForeignKey('User', on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.student} -> rating {self.rating}/10"
+    
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
